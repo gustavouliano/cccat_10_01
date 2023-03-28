ab1aa1891c180e21d395ae21611541ab9a0c6f4d
gustavouliano <gustavouliano11@gmail.com>
2023-03-27 22:15:45 -0300

    FreightCalculator refactor for product quantities

--- regras.md
+++ regras.md
@@ -43,3 +43,17 @@ Geladeira: 40kg / 1 m3 = 40kg/m3
 2 - Gerar o código/número de série do pedido (AAAASSSSSSSS)
 3 - Simulação do frete
 4 - Validar cupom de desconto
+
+## Parte 4
+
+1 - Deve retornar um pedido com base no código (caso de uso)
+2 - Deve retornar a lista de pedidos (caso de uso)
+3 - Calcule a distância entre dois CEPs e utilize no algoritmo de cálculo do frete
+4 - Calcular frete (caso de uso)
+
+Considere
+
+Criar uma API (tanto faz se é REST, GraphQL ou qualquer outro formato) para os casos de uso
+Criar testes de integração para a API e os casos de uso
+Cadastrar no banco de dados 2 ou 3 CEPs e associar com as coordenadas das cidades dos CEPs, apenas para viabilizar os cálculos, não é necessário carregar a base de CEPs inteira
+Utilizar o algoritmo de cálculo e arquivo com as coordenadas: https://github.com/rodrigobranas/distance_calculator
--- test/FreightCalculator.spec.ts
+++ test/FreightCalculator.spec.ts
@@ -1,8 +1,20 @@
 import FreightCalculator from "../src/domain/entity/FreightCalculator";
 import Product from "../src/domain/entity/Product";
 
-it('Deve calcular o frete do protudo', () => {
+it('Deve calcular o frete do produto de um item com quantidade 1', () => {
     const product = new Product(5, 'A', 1000, 100, 30, 10, 3, 'USD');
     const freight = FreightCalculator.calculate(product);
     expect(freight).toBe(30);
+})
+
+it('Deve calcular o frete do produto de um item com quantidade 3', () => {
+    const product = new Product(5, 'A', 1000, 100, 30, 10, 3, 'USD');
+    const freight = FreightCalculator.calculate(product, 3);
+    expect(freight).toBe(90);
+})
+
+it('Deve calcular o frete do produto com preço mínimo', () => {
+    const product = new Product(5, 'C', 1000, 10, 10, 10, 0.9, 'USD');
+    const freight = FreightCalculator.calculate(product);
+    expect(freight).toBe(10);
 })
